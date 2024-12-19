42e6c97613543a20a99531e535a3ef2d35267961
rajibcsit1 <18192203021@cse.bubt.edu.bd>
2024-12-19 12:56:07 +0600

    Post show and comment

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PostController extends Controller
 {
@@ -17,5 +19,56 @@ class PostController extends Controller
     {
         $posts = Post::latest()->get();
         return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Create a Posts of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        return back()->with('success', 'Post created successfully.');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Write code for comment
+     *
+     * @return response()
+     */
+    public function commentStore(Request $request)
+    {
+        $request->validate([
+            'body' => 'required',
+        ]);
+
+        $input = $request->all();
+        $input['user_id'] = auth()->user()->id;
+
+        Comment::create($input);
+
+        return back()->with('success', 'Comment added successfully.');
     }
 }
